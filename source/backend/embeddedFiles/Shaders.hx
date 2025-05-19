2cc75b072f0b0e9a8e250754a2a3f28d9b10e4e4
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2025-05-19 23:15:06 +0800

    Update Shaders.hx

--- source/backend/embeddedFiles/Shaders.hx
+++ source/backend/embeddedFiles/Shaders.hx
@@ -8,6 +8,15 @@ package backend.embeddedFiles;
 enum abstract Shaders(String) from String to String
 {
 
+	var gray = "
+        ////pragma header
+	void main() {
+		vec4 color = texture2D(bitmap, openfl_TextureCoordv);
+		float gray = dot(color.rgb, vec3(0.299, 0.587, 0.114));
+		gl_FragColor = vec4(vec3(gray), color.a);
+	}
+	";
+
 	var malfunctionBGEffect = 
 	"
 	 #pragma header
