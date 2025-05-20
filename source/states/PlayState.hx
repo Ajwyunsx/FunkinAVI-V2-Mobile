07e3800d3f7ee8556c9c5a2c149705fa03fda366
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2025-05-20 10:37:59 +0800

    Add files via upload

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -6674,7 +6674,7 @@ class PlayState extends MusicBeatState
 		{
 			if (!isAutoplay)
 			{
-				if (FlxG.keys.justPressed.SPACE #if android || _virtualpad.buttonD.justPressed #end)
+				if (FlxG.keys.justPressed.SPACE #if android || androidc.dodge.buttonD.justPressed #end)
 				{
 					/*
 					* This set is for song-specific gimmicks
